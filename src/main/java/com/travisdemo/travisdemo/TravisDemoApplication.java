e73d1435d6ec61a9b81069bd2de0eb159301791a
sanjeevblr <sanjeev.singh@traveloka.com>
2019-02-25 11:43:25 +0530

    Hello 2

--- src/main/java/com/travisdemo/travisdemo/TravisDemoApplication.java
+++ src/main/java/com/travisdemo/travisdemo/TravisDemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravisDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Test");
         SpringApplication.run(TravisDemoApplication.class, args);
+
     }
 
 }
